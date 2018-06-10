cddc7de674642ebeff905fa37fdf6e35900046bc
YukiLevin <yuki.arai.11.22@gmail.com>
2018-06-10 01:20:35 -0700

    Delete Comment

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,11 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MainComponent } from './main/main.component';
 import { InsertComponent } from './insert/insert.component';
-// import { SearchComponent } from './search/search.component';
 
 const myRoutes = [
     { path: 'insert', component: InsertComponent },
-//    { path: 'search', component: SearchComponent },
     { path: 'search', loadChildren: './search/search.module#SearchModule' },
     { path: '', component: MainComponent },
 ];
